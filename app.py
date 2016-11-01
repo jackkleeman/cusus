984cae4f86f3a296bf41b3bc56d3278a0f2ebf29
Jack Kleeman <Jack Kleeman>
2016-11-01 01:37:24 +0000

    change host to 0000

--- app.py
+++ app.py
@@ -103,7 +103,7 @@ if not app.debug:
 
 # Default port:
 if __name__ == "__main__":
-    app.run(debug=True, port=5000, use_reloader=True)
+    app.run(debug=True, host='0.0.0.0', port=5000, use_reloader=True)
 
 # Or specify port manually:
 '''
